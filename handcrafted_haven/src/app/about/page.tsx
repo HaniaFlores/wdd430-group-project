106439fcc7e77bd70b747ef91a5b49ef103fbec8
richardlyonheart <brendenlyon907@gmail.com>
2025-05-29 21:59:09 -0400

    erroneos file deleted

--- handcrafted_haven/src/app/about/page.tsx
+++ handcrafted_haven/src/app/about/page.tsx
@@ -3,7 +3,7 @@ import Footer from "@/components/footer";
 import Header from "@/components/header";
 
 
-export default async function Home() {
+export default async function Page() {
     return(
         <>
         <Header/>
